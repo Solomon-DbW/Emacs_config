2c292dbd294a3adb6c05319a41ad024007724421
Solomon Fosuhene <s2huncho@gmail.com>
2025-05-11 15:37:16 +0100

    Added more useful keybibdings and doom dark theme

--- config.el
+++ config.el
@@ -2,11 +2,13 @@
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1 :inherit ignore
-                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+(defvar elpaca-order
+  '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+           :ref nil :depth 1 :inherit ignore
+           :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+           :build (:not elpaca--activate-package)))
+
+(let* ((repo (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
@@ -29,36 +31,25 @@
                   ((elpaca-generate-autoloads "elpaca" repo)))
             (progn (message "%s" (buffer-string)) (kill-buffer buffer))
           (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive)))))
+(unless (require 'elpaca-autoloads nil t)
+  (require 'elpaca)
+  (elpaca-generate-autoloads "elpaca" repo)
+  (let ((load-source-file-function nil)) (load "./elpaca-autoloads")))
+
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
-;; Install a package via the elpaca macro
-;; See the "recipes" section of the manual for more details.
+(elpaca elpaca-use-package
+  (elpaca-use-package-mode))
 
-;; (elpaca example-package)
-
-(elpaca elpaca-use-package ;; Install use-package support
-     (elpaca-use-package-mode));; Enable use-package :ensure support for Elpaca.
-
-   ;;When installing a package used in the init file itself,
-   ;;e.g. a package which adds a use-package key word,
-   ;;use the :wait recipe keyword to block until that package is installed/configured.
-   ;;For example:
-   ;;(use-package general :ensure (:wait t) :demand t)
-
-   ;; Expands to: (elpaca evil (use-package evil :demand t))
 (elpaca evil
   (use-package evil
     :init
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil)
-    (setq evil-vsplit-window-right t)
-    (setq evil-split-window-below t)
+    (setq evil-want-integration t
+          evil-want-keybinding nil
+          evil-vsplit-window-right t
+          evil-split-window-below t)
     :config
     (evil-mode 1)))
 
@@ -72,58 +63,110 @@
 (elpaca evil-tutor
   (use-package evil-tutor))
 
-   ;;Turns off elpaca-use-package-mode current declaration
-   ;;Note this will cause evaluate the declaration immediately. It is not deferred.
-   ;;Useful for configuring built-in emacs features.
-   (use-package emacs :ensure nil :config (setq ring-bell-function #'ignore))
+(use-package emacs :ensure nil
+  :config
+  (setq ring-bell-function #'ignore))
 
 (elpaca general
   (use-package general
-  :config
-  (general-evil-setup)
+    :config
+    (general-evil-setup)
 
-  ;; Set 'SPC' as global leader key
-  (general-create-definer solomon/leader-keys
-			    :states '(normal insert visual emacs)
-		  :keymaps 'override
-		  :prefix "SPC" ;; setting leader key
-		  :global-prefix "M-SPC") ;; uses Alt-SPC to access leader
+    ;; Set 'SPC' as leader key
+    (general-create-definer solomon/leader-keys
+      :states '(normal insert visual emacs)
+      :keymaps 'override
+      :prefix "SPC"
+      :global-prefix "M-SPC")
 
   (solomon/leader-keys
-   "b" '(:ignore t:wk "buffer")
-   "bb" '(switch-to-buffer :wk "Switch buffer")
-   "bk" '(kill-this-buffer :wk "Kill this buffer")
-   "bn" '(next-buffer :wk "Next buffer")
-   "bp" '(previous-buffer :wk "Previous buffer")
-   "br" '(revert-buffer :wk "Reload buffer")
- )
-  ))
+	    "." '(find-file :wk "Find file")
+	    "f c" '((lambda () (interactive) (find-file "~/.config/emacs/config.org")) :wk "Edit emacs config")
+	    "g c c" '(comment-line :wk "Comment lines"))
+
+	(solomon/leader-keys
+	    "b" '(:ignore t :wk "buffer")
+	    "b b" '(switch-to-buffer :wk "Switch buffer")
+	    "b i" '(ibuffer :wk "Ibuffer")
+	    "b k" '(kill-this-buffer :wk "Kill buffer")
+	    "b n" '(next-buffer :wk "Next buffer")
+	    "b p" '(previous-buffer :wk "Previous buffer")
+	    "b r" '(revert-buffer :wk "Reload buffer")
+
+	    "e" '(:ignore t :wk "evaluate")
+	    "e b" '(eval-buffer :wk "Eval buffer")
+	    "e d" '(eval-defun :wk "Eval defun")
+	    "e e" '(eval-expression :wk "Eval expression")
+	    "e l" '(eval-last-sexp :wk "Eval last sexp")
+	    "e r" '(eval-region :wk "Eval region")
+
+	    "h" '(:ignore t :wk "Help")
+	    "h f" '(describe-function :wk "Describe function")
+	    "h v" '(describe-variable :wk "Describe variable")
+	    "h r r" '((lambda () (interactive) (load-file "~/.config/emacs/init.el")) :wk "Reload emacs config")
+	    ;; "h r r" '(reload-init-file :wk "Reload emacs config")
+	  )))
 
 (set-face-attribute 'default nil
-		    :font "JetBrains Mono"
-		    :height 110
-		  :weight 'medium)
+                    :font "JetBrains Mono"
+                    :height 110
+                    :weight 'medium)
+
 (set-face-attribute 'variable-pitch nil
-		    :font "JetBrains Mono"
-		    :height 120
-		  :weight 'medium)
+                    :font "JetBrains Mono"
+                    :height 120
+                    :weight 'medium)
+
 (set-face-attribute 'fixed-pitch nil
-		    :font "JetBrains Mono"
-		    :height 110
-		  :weight 'medium)
+                    :font "JetBrains Mono"
+                    :height 110
+                    :weight 'medium)
 
-(set-face-attribute 'font-lock-comment-face nil
-		    :slant 'italic)
-;; ✅ Fix: quote 'italic to prevent void variable error
-(set-face-attribute 'font-lock-keyword-face nil
-		    :slant 'italic)
-
-
+(set-face-attribute 'font-lock-comment-face nil :slant 'italic)
+(set-face-attribute 'font-lock-keyword-face nil :slant 'italic)
 
 (add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
 
-;; Uncomment this line if line spacing needs adjusting
 (setq-default line-spacing 0.12)
+
+(use-package dashboard
+  :ensure t 
+  :init
+  (setq initial-buffer-choice 'dashboard-open
+        dashboard-set-heading-icons t
+        dashboard-set-file-icons t
+        dashboard-banner-logo-title "Emacs Is More Than A Text Editor!"
+        dashboard-startup-banner "~/.config/emacs/images/emacs-dash.png"
+        dashboard-center-content nil
+        dashboard-items '((recents . 5)
+                          (agenda . 5)
+                          (bookmarks . 3)
+                          (projects . 3)
+                          (registers . 3)))
+  :custom
+  (dashboard-modify-heading-icons '((recents . "file-text")
+                                     (bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+  ;; (invert-face 'default)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -132,53 +175,28 @@
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
 
-(set-face-attribute 'default nil
-		    :font "JetBrains Mono"
-		    :height 110
-		  :weight 'medium)
-(set-face-attribute 'variable-pitch nil
-		    :font "JetBrains Mono"
-		    :height 120
-		  :weight 'medium)
-(set-face-attribute 'fixed-pitch nil
-		    :font "JetBrains Mono"
-		    :height 110
-		  :weight 'medium)
-
-(set-face-attribute 'font-lock-comment-face nil
-		    :slant 'italic)
-(set-face-attribute 'font-lock-keyword-face nil
-		    :slant 'italic)
-
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
-
-;; Uncommentthis line if line spacing needs adjusting
-(setq-default line-spacing 0.12)
-
 (elpaca toc-org
-(use-package toc-org
+  (use-package toc-org
     :commands toc-org-enable
-    :init (add-hook 'org-mode-hook 'toc-org-enable)))
+    :init
+    (add-hook 'org-mode-hook 'toc-org-enable)))
 
 (use-package org-bullets
-:ensure t
-:hook (org-mode . org-bullets-mode))
+  :ensure t
+  :hook (org-mode . org-bullets-mode))
 
-;;(elpaca which-key
 (use-package which-key
-  :init
-    (which-key-mode 1)
+  :init (which-key-mode 1)
   :config
   (setq which-key-side-window-location 'bottom
-	  which-key-sort-order #'which-key-key-order-alpha
-	  which-key-sort-uppercase-first nil
-	  which-key-add-column-padding 1
-	  which-key-max-display-columns nil
-	  which-key-min-display-lines 6
-	  which-key-side-window-slot -10
-	  which-key-idle-delay 0.1
-	  which-key-side-window-max-height 0.25
-	  which-key-max-description-length 25
-	  which-key-allow-imprecise-window-fit t
-	  which-key-separator " → " ))
-;;)
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-idle-delay 0.1
+        which-key-side-window-max-height 0.25
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit t
+        which-key-separator " → "))
