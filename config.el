3dcbb011e2aafb646dc079c9057c30f16b653cf0
Solomon Fosuhene <s2huncho@gmail.com>
2025-05-11 13:54:17 +0100

    Got simple config working

--- config.el
+++ config.el
@@ -42,10 +42,8 @@
 
 ;; (elpaca example-package)
 
-;; Install use-package support
-   (elpaca elpaca-use-package
-     ;; Enable use-package :ensure support for Elpaca.
-     (elpaca-use-package-mode))
+(elpaca elpaca-use-package ;; Install use-package support
+     (elpaca-use-package-mode));; Enable use-package :ensure support for Elpaca.
 
    ;;When installing a package used in the init file itself,
    ;;e.g. a package which adds a use-package key word,
@@ -78,3 +76,109 @@
    ;;Note this will cause evaluate the declaration immediately. It is not deferred.
    ;;Useful for configuring built-in emacs features.
    (use-package emacs :ensure nil :config (setq ring-bell-function #'ignore))
+
+(elpaca general
+  (use-package general
+  :config
+  (general-evil-setup)
+
+  ;; Set 'SPC' as global leader key
+  (general-create-definer solomon/leader-keys
+			    :states '(normal insert visual emacs)
+		  :keymaps 'override
+		  :prefix "SPC" ;; setting leader key
+		  :global-prefix "M-SPC") ;; uses Alt-SPC to access leader
+
+  (solomon/leader-keys
+   "b" '(:ignore t:wk "buffer")
+   "bb" '(switch-to-buffer :wk "Switch buffer")
+   "bk" '(kill-this-buffer :wk "Kill this buffer")
+   "bn" '(next-buffer :wk "Next buffer")
+   "bp" '(previous-buffer :wk "Previous buffer")
+   "br" '(revert-buffer :wk "Reload buffer")
+ )
+  ))
+
+(set-face-attribute 'default nil
+		    :font "JetBrains Mono"
+		    :height 110
+		  :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+		    :font "JetBrains Mono"
+		    :height 120
+		  :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+		    :font "JetBrains Mono"
+		    :height 110
+		  :weight 'medium)
+
+(set-face-attribute 'font-lock-comment-face nil
+		    :slant 'italic)
+;; ✅ Fix: quote 'italic to prevent void variable error
+(set-face-attribute 'font-lock-keyword-face nil
+		    :slant 'italic)
+
+
+
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+
+;; Uncomment this line if line spacing needs adjusting
+(setq-default line-spacing 0.12)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+(global-visual-line-mode t)
+
+(set-face-attribute 'default nil
+		    :font "JetBrains Mono"
+		    :height 110
+		  :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+		    :font "JetBrains Mono"
+		    :height 120
+		  :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+		    :font "JetBrains Mono"
+		    :height 110
+		  :weight 'medium)
+
+(set-face-attribute 'font-lock-comment-face nil
+		    :slant 'italic)
+(set-face-attribute 'font-lock-keyword-face nil
+		    :slant 'italic)
+
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+
+;; Uncommentthis line if line spacing needs adjusting
+(setq-default line-spacing 0.12)
+
+(elpaca toc-org
+(use-package toc-org
+    :commands toc-org-enable
+    :init (add-hook 'org-mode-hook 'toc-org-enable)))
+
+(use-package org-bullets
+:ensure t
+:hook (org-mode . org-bullets-mode))
+
+;;(elpaca which-key
+(use-package which-key
+  :init
+    (which-key-mode 1)
+  :config
+  (setq which-key-side-window-location 'bottom
+	  which-key-sort-order #'which-key-key-order-alpha
+	  which-key-sort-uppercase-first nil
+	  which-key-add-column-padding 1
+	  which-key-max-display-columns nil
+	  which-key-min-display-lines 6
+	  which-key-side-window-slot -10
+	  which-key-idle-delay 0.1
+	  which-key-side-window-max-height 0.25
+	  which-key-max-description-length 25
+	  which-key-allow-imprecise-window-fit t
+	  which-key-separator " → " ))
+;;)
---
+++ elpaca/builds/which-key/which-key.el
@@ -0,0 +1,1 @@
+/home/solomon/.config/emacs/elpaca/repos/emacs-which-key/which-key.el
